975bba84a04ebb14f85530539ef9270f13d94cba
fmshot2 <femishotolaa@gmail.com>
2022-10-26 06:01:20 +0100

    set up

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import "./assets/css/color4.css";
+import Header from "./components/layout/Header";
+function App() {
+  return (
+    <div className="App">
+      <Header />
+    </div>
+  );
+}
+
+export default App;
